54a6f5ca2191fb42212b110bef4b15f7bdd39efe
Nilesh Teji <kumar.nilesh023@gmail.com>
2021-03-06 10:24:52 +0530

    Project Template Creation

---
+++ app/src/main/java/com/tyba/obvioassignment/core/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tyba.obvioassignment.core
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tyba.obvioassignment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
